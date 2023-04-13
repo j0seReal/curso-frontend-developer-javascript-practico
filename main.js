6ca8d839a4a8db550081d45ce399bdbc67b42d61
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-04-12 20:03:54 -0700

    Clase

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
-const navEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
-navEmail.addEventListener('click', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleCartAsside);
 
 function toggleDesktopMenu() {
+   
+  desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
     
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAsside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    } 
+
+    aside.classList.toggle('inactive');
+}
+
+
+
+
+
+/*
+const navEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+menuIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    console.log('click');
+    mobileMenu.classList.toggle('inactive');
+}
+*/
